27e54a244c711e054c081213e1014a215b4f6700
= <suryanshdwivedi39@gmail.com>
2026-04-29 10:06:47 +0530

    add

---
+++ Python/Average_of_two_number.py
@@ -0,0 +1,7 @@
+# To take first number input 
+num1 = int(input("Enter first number: "))
+# To take second number input
+num2 = int(input("Enter second number: ")) 
+
+Average = (num1 + num2) / 2
+print(Average) 
---
+++ Python/Sum_of_number.py
@@ -0,0 +1,7 @@
+num1 = 10
+num2 = 20
+
+sum = num1 + num2 
+
+print("Sum is:", sum)   
+
